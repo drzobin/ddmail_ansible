64095113c09c22e1bd33363f61e848704ca3edd1
drz <robin@ddmail.se>
2025-10-11 15:47:41 +0200

    Add type hint

--- gen_vault.py
+++ gen_vault.py
@@ -1,4 +1,5 @@
 """Generate ansible vault file from template file"""
+
 import argparse
 import os
 import string
@@ -7,94 +8,94 @@ import sys
 from argon2 import PasswordHasher
 
 
-def gen_hash(string):
+def gen_hash(string: str) -> str:
     """Generate argon2i hash from string."""
     ph = PasswordHasher()
-    argon2i_hash = ph.hash(string)
+    argon2i_hash = str(ph.hash(string))
 
     return argon2i_hash
 
 
-def gen_pass(length):
+def gen_pass(length: int) -> str:
     """Generate a password."""
     alphabet = string.ascii_letters + string.digits
     while True:
-        password = ''.join(secrets.choice(alphabet) for i in range(length))
-        if (any(c.islower() for c in password) and
-            any(c.isupper() for c in password) and
-            sum(c.isdigit() for c in password) >= 3):
+        password = "".join(secrets.choice(alphabet) for i in range(length))
+        if (
+            any(c.islower() for c in password)
+            and any(c.isupper() for c in password)
+            and sum(c.isdigit() for c in password) >= 3
+        ):
             break
     return password
 
 
-def gen_key(length):
+def gen_key(length: int) -> str:
     """Generate a key in byte from"""
     key = secrets.token_hex(length)
 
     return key
 
-def parse_key(file):
+
+def parse_key(file: str) -> str:
     """Parse OpenPGP and ssh key files"""
     # Read the data from file.
     f = open(file, "r")
     data = f.read()
     f.close()
 
-    data = data.replace("\n","\\n")
-    data = "\"" + data + "\""
+    data = data.replace("\n", "\\n")
+    data = '"' + data + '"'
 
     return data
+
 
 if __name__ == "__main__":
     """Main function """
 
     # Get arguments from args.
     parser = argparse.ArgumentParser(
-            description="Generate ansible vault file from template file"
-            )
+        description="Generate ansible vault file from template file"
+    )
 
     parser.add_argument(
-            '-t',
-            '--template-file',
-            type=str,
-            help='Input template file',
-            required=True
-            )
+        "-t",
+        "--template-file",
+        type=str,
+        help="Input template file",
+        required=True,
+    )
     parser.add_argument(
-            '-o',
-            '--output-file',
-            type=str,
-            help='Output vault file',
-            required=True
-            )
+        "-o", "--output-file", type=str, help="Output vault file", required=True
+    )
     parser.add_argument(
-            '-btpub',
-            '--backup-taker-pubkey',
-            type=str,
-            help='OpenPGP armored public key file used for encrypting backups with backup_taker',
-            required=True
-            )
+        "-btpub",
+        "--backup-taker-pubkey",
+        type=str,
+        help="OpenPGP armored public key file used for encrypting backups with backup_taker",
+        required=True,
+    )
     parser.add_argument(
-            '-btpf',
-            '--backup-taker-pubkey-fingerprint',
-            type=str,
-            help='OpenPGP fingerprint of public key used for encrypting backups with backup_taker',
-            required=True
-            )
+        "-btpf",
+        "--backup-taker-pubkey-fingerprint",
+        type=str,
+        help="OpenPGP fingerprint of public key used for encrypting backups with backup_taker",
+        required=True,
+    )
     parser.add_argument(
-            '-gspub',
-            '--github-ssh-pubkey',
-            type=str,
-            help='Developers github ssh public key',
-            required=False
-            )
+        "-gspub",
+        "--github-ssh-pubkey",
+        type=str,
+        help="Developers github ssh public key",
+        required=False,
+    )
     parser.add_argument(
-            '-gspriv',
-            '--github-ssh-privkey',
-            type=str,
-            help='Developers github ssh private key',
-            required=False
-            )
+        "-gspriv",
+        "--github-ssh-privkey",
+        type=str,
+        help="Developers github ssh private key",
+        required=False,
+    )
 
     args = parser.parse_args()
 
@@ -139,26 +140,30 @@ if __name__ == "__main__":
             f.write(line)
             save = None
         elif "vault_mailsystem_db_dev_user_password_hash" in line:
-            argon2i_hash = gen_hash("A"*24)
+            argon2i_hash = gen_hash("A" * 24)
             line = line.replace("change_me_to_argon2_hash", argon2i_hash)
 
             f.write(line)
             save = None
         elif "vault_mailsystem_db_dev_user_file_hash" in line:
-            argon2i_hash = gen_hash("A"*4096)
+            argon2i_hash = gen_hash("A" * 4096)
             line = line.replace("change_me_to_argon2_hash", argon2i_hash)
 
             f.write(line)
             save = None
         elif "vault_mailsystem_db_dev_email_hash" in line:
-            none_default_ph = PasswordHasher(time_cost=3,memory_cost=65536,parallelism=1)
-            argon2i_hash = none_default_ph.hash("A"*24)
+            none_default_ph = PasswordHasher(
+                time_cost=3, memory_cost=65536, parallelism=1
+            )
+            argon2i_hash = none_default_ph.hash("A" * 24)
             line = line.replace("change_me_to_argon2_hash", argon2i_hash)
 
             f.write(line)
             save = None
         elif "vault_ddmail_backup_taker_pubkey_fingerprint" in line:
-            line = line.replace("change_me", args.backup_taker_pubkey_fingerprint)
+            line = line.replace(
+                "change_me", args.backup_taker_pubkey_fingerprint
+            )
 
             f.write(line)
             save = None
@@ -169,7 +174,10 @@ if __name__ == "__main__":
 
             f.write(data)
             save = None
-        elif "vault_github_ssh_pubkey" in line and args.github_ssh_pubkey is not None:
+        elif (
+            "vault_github_ssh_pubkey" in line
+            and args.github_ssh_pubkey is not None
+        ):
             # Check that file exsist and is a file.
             if os.path.isfile(args.github_ssh_pubkey) is False:
                 print("Error: Github ssh pubkey file can not be found")
@@ -180,7 +188,10 @@ if __name__ == "__main__":
             data = "vault_github_ssh_pubkey: " + pub_key_data + "\n"
             f.write(data)
             save = None
-        elif "vault_github_ssh_privkey" in line and args.github_ssh_privkey is not None:
+        elif (
+            "vault_github_ssh_privkey" in line
+            and args.github_ssh_privkey is not None
+        ):
             # Check that file exsist and is a file.
             if os.path.isfile(args.github_ssh_privkey) is False:
                 print("Error: Github ssh privkey file can not be found")
